b49e98191606628edd05f04876591c3b8703849d
Mary62442 <maria.bu62442@gmail.com>
2021-03-04 16:00:35 +0100

    Changed favicon, added google analytics, and linkedin icon

--- static.config.js
+++ static.config.js
@@ -2,6 +2,15 @@ import path from 'path'
 import axios from 'axios'
 import React, {Component} from 'react'
 
+const gTag = `
+		window.dataLayer = window.dataLayer || [];
+		function gtag() {
+			window.dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+		gtag('config', 'G-E1NCEQ3168');
+`;
+
 export default {
 
   getSiteProps: async () => ({
@@ -18,10 +27,14 @@ export default {
               <meta name="viewport" content="width=device-width, initial-scale=1"/>
               <title>ProActivity</title>
               <meta name="description" content="We provide extraordinary value to companies through a robust team of experienced IT Professionals specialized in the development of enterprise-level software solutions."/>
+              
               <link rel="preconnect" href="https://fonts.gstatic.com"/>
               <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"/>
               <meta property="og:image" content="http://localhost:3000/icon.png"/>
               <link rel="icon" href="/icon.png"/>
+              <script async src="https://www.googletagmanager.com/gtag/js?id=G-E1NCEQ3168"></script>
+              <script dangerouslySetInnerHTML={{__html: gTag}}/>
+
             </Head>
             <Body>
               {children}
