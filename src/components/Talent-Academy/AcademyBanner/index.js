c815b3b013d57da606bcbe2cff9a8b600317bacb
Lucabudi <39259808+Lucabudi@users.noreply.github.com>
2022-08-29 14:23:53 +0100

    Feature/fix text (#8)
    * fix
    
    * fix text and logo
    
    * height banner
    
    * and link

--- src/components/Talent-Academy/AcademyBanner/index.js
+++ src/components/Talent-Academy/AcademyBanner/index.js
@@ -7,7 +7,7 @@ function AcademyBanner(){
             <div className="content" >
                 <div className="academy-text" style={{display: 'inline-flex', gap: '1rem', alignItems: 'center'}}>
                     <h2><b>TALENT ACADEMY</b></h2> 
-                    <p style={{ fontSize: 16, paddingLeft: '0%', marginBottom: 0,}}>Il nostro nuovo percorso formativo per i giovani talenti del mondo IT. </p>
+                    <p style={{ fontSize: 16, paddingLeft: '0%', marginBottom: 0,}}>Il nostro nuovo <a style={{ display: 'inline-flex', textDecoration: 'underline', color: 'inherit'}} href="/talent-academy">percorso formativo</a> per i giovani talenti del mondo IT. </p>
                 </div>
                 <div className="candidatura">
                     <div className="application-text" style={{display: 'inline-flex', gap: '1rem'}}>
