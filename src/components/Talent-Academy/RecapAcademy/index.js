e5215a79386f522baee508a45a4f976f50254d6f
Lucabudi <39259808+Lucabudi@users.noreply.github.com>
2022-09-19 11:06:02 +0200

    Feature/fix landing (#12)
    * landing new
    
    * fix

--- src/components/Talent-Academy/RecapAcademy/index.js
+++ src/components/Talent-Academy/RecapAcademy/index.js
@@ -6,15 +6,7 @@ function RecapAcademy() {
     return(
         <div className="box-recap">
             <div className="text">
-            ISCRIVITI ALLA NOSTRA <br />TALENT ACADEMY. <br/> IL PRIMO CORSO DI <br/> FORMAZIONE PER VERI <br/> CODE-ADDICTED!
-            <br /><a target="_blank" href="https://www.linkedin.com/jobs/view/3233818092/?refId=WxpwjZn4MS2oNwKk4wdXvg%3D%3D&trackingId=dBAAxTtwmImt7Mctf5ynQQ%3D%3D"><button className="button">ISCRIVITI ORA</button></a>
-            </div>
-            <div className="info">
-                <p>DATA INZIO DEL CORSO <br/> <b>Mercoledì 28 settembre 2022</b></p>
-                <p>DATA DI FINE CORSO <br/> <b>Giovedì 13 ottobre 2022</b></p>
-                <p>DURATA <br/> <b>70 ore</b></p>
-                <p>POSTI MASSIMI  <br/> <b>8</b></p>
-                <p>DEADLINE INVIO DOMANDA  <br/> <b>Venerdì 16 settembre 2022</b></p>
+                <p>Seguici sul nostro sito e sui nostri canali social per scoprire tutti i nuovi corsi in partenza nei prossimi mesi!</p>
             </div>
          </div>
     )
